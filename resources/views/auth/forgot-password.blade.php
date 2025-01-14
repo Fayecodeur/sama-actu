f34fdd5ee75b6c774421be8ae6ffbcb314db6618
Fayecodeur <fayecodeur99@gmail.com>
2025-01-14 15:17:49 +0000

    modification du formulaire de rénitialisation mdp

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,26 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@extends('layouts.auth')
+@section('title', "Rénitiliser mot de passe")
+@section('content')
+    <h1>Mot de passe oublie?</h1>
+    <p class="account-subtitle">Entrer votre email pour obtenier le lien de reunitialisation</p>
+    @if( session('status'))
+        <div class="alert alert-success">
+            {{session('status')}}
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+    @endif
+    <form action="{{route('password.email')}}" method="POST">
+        @csrf
+        <div class="form-group">
+            <input class="form-control" type="email" name="email" value="{{old('email')}}" placeholder="Email">
+            @error('email')
+            <span class="text-danger">{{$message}}</span>
+            @enderror
+        </div>
+        <div class="form-group mb-0">
+            <button class="btn btn-primary btn-block" type="submit">Recevoir le lien</button>
         </div>
     </form>
-</x-guest-layout>
+    <div class="text-center dont-have">Vous vous souvenez de votre mot de passe? <a
+            href="{{route('login')}}">Se connecter</a>
+    </div>
+@endsection
