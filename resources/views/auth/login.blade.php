bc04cffcfdaac545edc5c39515f5bcaa5eae06ff
Fayecodeur <fayecodeur99@gmail.com>
2025-01-14 15:18:41 +0000

    modification du formulaire de connexion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,24 @@
 @section('content')
     <h1>Connexion</h1>
     <p class="account-subtitle">Acceder au dashboard</p>
+    @if( session('status'))
+        <div class="alert alert-success">
+            {{session('status')}}
+        </div>
+    @endif
     <form action="{{route('login')}}" method="POST">
         @csrf
         @method('POST')
         <div class="form-group">
             <input class="form-control" type="email" name="email" value="{{old('email')}}" placeholder="Email">
             @error('email')
-            <span class="text-red-500">{{$message}}</span>
+            <span class="text-danger">{{$message}}</span>
             @enderror
         </div>
         <div class="form-group">
             <input class="form-control" type="password" name="password" value="{{old('password')}}" placeholder="Mot de passe">
             @error('password')
-            <span class="text-red-500">{{$message}}</span>
+            <span class="text-danger">{{$message}}</span>
             @enderror
         </div>
         <div class="form-group">
@@ -23,7 +28,7 @@
         </div>
     </form>
     <div class="text-center forgotpass">
-        <a href="forgot-password.html">Mot de passe oublie?</a>
+        <a href="{{route('password.request')}}">Mot de passe oublie?</a>
     </div>
     <div class="text-center dont-have">
         Vous n'avez pas de compt?
