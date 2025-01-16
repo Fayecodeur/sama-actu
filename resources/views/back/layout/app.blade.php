f64e47c377bd40609b9647c14709bbb2fb39d5f4
Fayecodeur <fayecodeur99@gmail.com>
2025-01-16 13:17:58 +0000

    ajout de commentaires

--- resources/views/back/layout/app.blade.php
+++ resources/views/back/layout/app.blade.php
@@ -7,21 +7,21 @@
       content="width=device-width, initial-scale=1.0, user-scalable=0"
     />
     <title>@yield('title')</title>
-    {# Dashboard - Links #}
+    {{--  Dashboard - Links  --}}
     @include('back.partials.styles')
-    {# Fin Dashbord Link #}
+    {{--  Fin Dashbord Link  --}}
   </head>
 
   <body>
-    {# Main wrapper #}
+    {{-- {# Main wrapper #} --}}
     <div class="main-wrapper">
-      {# Debut Header #}
+      {{-- {# Debut Header #} --}}
         @include('back.components.header')
-      {# Fin Header #} 
+      {{-- {# Fin Header #} 
       {# ------------------ #}
-      {# Debut Sidebar #}
+      {# Debut Sidebar #} --}}
         @include('back.components.sidebar')
-      {# Fin Sidebar #} {# --------------------- #} {# Contenu de la page #}
+      {{-- {# Fin Sidebar #} {# --------------------- #} {# Contenu de la page #} --}}
       <div class="page-wrapper">
         <div class="content container-fluid">
           <div class="page-header">
@@ -30,10 +30,10 @@
           @yield('content')
         </div>
       </div>
-      {# Fin Contenu de la page #}
+      {{-- {# Fin Contenu de la page #} --}}
     </div>
-    {# Scripts dashboard #}
+    {{-- {# Scripts dashboard #} --}}
       @include('back.partials.scripts')
-    {# Fin Script Dashboard #}
+    {{-- {# Fin Script Dashboard #} --}}
   </body>
 </html>
